a4e2bf9bcebf3d8d2cda042cdf0b9b74d8e20c4f
Pascalcytech <pascal.kau8@gmail.com>
2024-11-04 14:26:29 +0100

    [Functionnality] Change list of sockets.
    New POST method is needed.

--- src/main/java/app/controller/SocketController.java
+++ src/main/java/app/controller/SocketController.java
@@ -3,6 +3,7 @@ package app.controller;
 import app.entity.Socket;
 import app.service.SocketService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -34,5 +35,12 @@ public class SocketController {
         // Check if socket is present and return it, otherwise return 404 Not Found
         return socket.map(ResponseEntity::ok)
                      .orElseGet(() -> ResponseEntity.notFound().build());
+    }
+    
+    // Not mapping was made for the POST method ...
+    @PostMapping
+    public ResponseEntity<Socket> createSocket(@RequestBody Socket socket) {
+        Socket createdSocket = socketService.createSocket(socket);
+        return ResponseEntity.status(HttpStatus.CREATED).body(createdSocket);
     }
 }
